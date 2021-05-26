dc2d816eed8f29b2f79bd0b9270cba1307a3ef21
Scala Steward <me@scala-steward.org>
2021-05-26 12:46:15 +0200

    Update circe-core, circe-generic, ... to 0.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
 
   object App {
     val http4sVersion = "0.21.22"
-    val circeVersion = "0.13.0"
+    val circeVersion = "0.14.0"
     val http4s = "org.http4s" %% "http4s-core" % http4sVersion
     val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
     val http4sServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
