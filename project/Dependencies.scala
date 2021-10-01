9e95e8797f3ebd441f94283fccd623a955777489
Scala Steward <me@scala-steward.org>
2021-10-01 20:28:25 +0200

    Update metrics-jmx to 4.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -76,7 +76,7 @@ object Dependencies {
 
     val cassandraDriverCore = "com.datastax.oss" % "java-driver-core" % cassandraDriverVersion
     val cassandraDriverQueryBuilder = "com.datastax.oss" % "java-driver-query-builder" % cassandraDriverVersion
-    val cassandraDriverMetrics = "io.dropwizard.metrics" % "metrics-jmx" % "4.2.0"
+    val cassandraDriverMetrics = "io.dropwizard.metrics" % "metrics-jmx" % "4.2.4"
     val akkaPersistenceCassandra = "com.typesafe.akka" %% "akka-persistence-cassandra" % akkaPersistenceCassandraVersion
     val cassandraUnit = "org.cassandraunit" % "cassandra-unit" % "4.3.1.0"
 
