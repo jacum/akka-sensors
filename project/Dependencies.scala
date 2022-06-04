b4e8cb955fcb9f475ddf4417b0a3040d22b1d8d9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-06-04 11:49:48 +0200

    Update scalatest to 3.2.12 (#213)
    Co-authored-by: Tim Evdokimov <tim@pragmasoft.nl>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -94,7 +94,7 @@ object Dependencies {
 
   object TestTools {
     val log       = "ch.qos.logback" % "logback-classic" % "1.2.10"
-    val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.11"
+    val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.12"
     val deps      = Logging.deps ++ testDeps(scalaTest, akkaInmemoryJournal, log)
   }
 
