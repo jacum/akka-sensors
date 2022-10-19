ee6e0a8dcd5310145941052367d15302a4c51276
Scala Steward <scala_steward@virtuslab.com>
2022-10-19 19:58:29 +0000

    Update logback-classic to 1.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -87,7 +87,7 @@ object Dependencies {
   }
 
   object TestTools {
-    val log       = "ch.qos.logback" % "logback-classic" % "1.4.1"
+    val log       = "ch.qos.logback" % "logback-classic" % "1.4.4"
     val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.13"
     val deps      = Logging.deps ++ testDeps(scalaTest, akkaInmemoryJournal, log)
   }
