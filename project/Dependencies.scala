051c45846657069579997f952fed0b79cbb24367
Scala Steward <scala_steward@virtuslab.com>
2023-06-04 19:08:43 +0000

    Update metrics-jmx to 4.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,18 +52,18 @@ object Dependencies {
 
   object Http4s {
     // unfortunately, http4s modules' versions not synced anymore
-    val http4sVersionBase = "0.23.13"
+    val http4sVersionBase    = "0.23.13"
     val http4sVersionModules = "0.23.18"
     val http4sVersionMetrics = "0.24.3"
-    val server              = "org.http4s"       %% "http4s-blaze-server"       % http4sVersionBase
-    val client              = "org.http4s"       %% "http4s-blaze-client"       % http4sVersionBase
-    val jdkClient           = "org.http4s"       %% "http4s-jdk-http-client"    % "0.7.0"
-    val xml                 = "org.http4s"       %% "http4s-scala-xml"          % http4sVersionBase
-    val circe               = "org.http4s"       %% "http4s-circe"              % http4sVersionModules
-    val dsl                 = "org.http4s"       %% "http4s-dsl"                % http4sVersionModules
-    val metrics             = "org.http4s"       %% "http4s-prometheus-metrics" % http4sVersionMetrics
-    val prometheusJmx       = "io.prometheus.jmx" % "collector"                 % "0.17.2"
-    val deps: Seq[ModuleID] = Seq(server, client, circe, xml, dsl, metrics, prometheusJmx)
+    val server               = "org.http4s"       %% "http4s-blaze-server"       % http4sVersionBase
+    val client               = "org.http4s"       %% "http4s-blaze-client"       % http4sVersionBase
+    val jdkClient            = "org.http4s"       %% "http4s-jdk-http-client"    % "0.7.0"
+    val xml                  = "org.http4s"       %% "http4s-scala-xml"          % http4sVersionBase
+    val circe                = "org.http4s"       %% "http4s-circe"              % http4sVersionModules
+    val dsl                  = "org.http4s"       %% "http4s-dsl"                % http4sVersionModules
+    val metrics              = "org.http4s"       %% "http4s-prometheus-metrics" % http4sVersionMetrics
+    val prometheusJmx        = "io.prometheus.jmx" % "collector"                 % "0.17.2"
+    val deps: Seq[ModuleID]  = Seq(server, client, circe, xml, dsl, metrics, prometheusJmx)
   }
 
   object App {
@@ -79,7 +79,7 @@ object Dependencies {
 
     val cassandraDriverCore         = "com.datastax.oss"      % "java-driver-core"           % cassandraDriverVersion
     val cassandraDriverQueryBuilder = "com.datastax.oss"      % "java-driver-query-builder"  % cassandraDriverVersion
-    val cassandraDriverMetrics      = "io.dropwizard.metrics" % "metrics-jmx"                % "4.2.15"
+    val cassandraDriverMetrics      = "io.dropwizard.metrics" % "metrics-jmx"                % "4.2.19"
     val akkaPersistenceCassandra    = "com.typesafe.akka"    %% "akka-persistence-cassandra" % akkaPersistenceCassandraVersion
     val cassandraUnit               = "org.cassandraunit"     % "cassandra-unit"             % "4.3.1.0"
 
