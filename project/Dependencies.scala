219dd019b32876461c7c4a0780a51b3b8d275de8
Scala Steward <scala_steward@virtuslab.com>
2023-09-12 19:08:41 +0000

    Update scalatest to 3.2.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -87,7 +87,7 @@ object Dependencies {
 
   object TestTools {
     val log       = "ch.qos.logback" % "logback-classic" % "1.4.8"
-    val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.16"
+    val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.17"
     val deps      = Logging.deps ++ testDeps(scalaTest, akkaInmemoryJournal, log)
   }
 
