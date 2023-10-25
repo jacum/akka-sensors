b441e94d3b862db22836ff53d1bba2315e694a8e
Aleksei Shamenev <coffius@gmail.com>
2023-10-25 09:59:43 +0300

    remove unused `akka-persistence-inmemory`

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,12 +75,6 @@ object Dependencies {
   }
 
   object TestTools {
-    val akkaInmemoryJournal = ("com.github.dnvriend" %% "akka-persistence-inmemory" % "2.5.15.2")
-      .exclude("com.typesafe.akka", "akka-actor")
-      .exclude("com.typesafe.akka", "akka-persistence")
-      .exclude("com.typesafe.akka", "akka-persistence-query")
-      .exclude("com.typesafe.akka", "akka-stream")
-      .exclude("com.typesafe.akka", "akka-protobuf")
     val log       = "ch.qos.logback" % "logback-classic" % "1.4.8"
     val scalaTest = "org.scalatest" %% "scalatest"       % "3.2.16"
     val deps      = Logging.deps ++ testDeps(scalaTest, log)
