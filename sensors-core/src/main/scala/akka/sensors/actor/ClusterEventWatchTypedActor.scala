af1d9e4a336046a7858cad99def4ddc85d5b4b89
Aleksei Shamenev <coffius@gmail.com>
2024-02-08 12:52:06 +0200

    fix subscription in ClusterEventWatchTypedActor

--- sensors-core/src/main/scala/akka/sensors/actor/ClusterEventWatchTypedActor.scala
+++ sensors-core/src/main/scala/akka/sensors/actor/ClusterEventWatchTypedActor.scala
@@ -7,41 +7,41 @@ import akka.cluster.Member
 import akka.cluster.typed.{Cluster, Subscribe, Unsubscribe}
 import io.prometheus.client.Counter
 object ClusterEventWatchTypedActor {
-  def apply(clusterEvents: Counter): Behavior[ClusterDomainEvent] = Behaviors.setup { context =>
-    val cluster    = Cluster(context.system)
-    val log        = context.log
-    val meterEvent = registerEvent(clusterEvents) _
+  def apply(clusterEvents: Counter): Behavior[ClusterDomainEvent] =
+    Behaviors.setup { context =>
+      val cluster    = Cluster(context.system)
+      val log        = context.log
+      val meterEvent = registerEvent(clusterEvents) _
 
-    Behaviors
-      .receiveMessage[ClusterDomainEvent] { event =>
-        event match {
-          case e @ MemberUp(member) =>
-            meterEvent(e, Some(member))
-            log.info("Member is Up: {}", member.address)
-          case e @ UnreachableMember(member) =>
-            meterEvent(e, Some(member))
-            log.info("Member detected as unreachable: {}", member)
-          case e @ MemberRemoved(member, previousStatus) =>
-            meterEvent(e, Some(member))
-            log.info("Member is Removed: {} after {}", member.address, previousStatus)
-          case e @ MemberDowned(member) =>
-            meterEvent(e, Some(member))
-            log.info("Member is Down: {}", member.address)
-          case e =>
-            meterEvent(e, None)
-            log.info(s"Cluster domain event: $e")
+      cluster.subscriptions ! Subscribe(context.self, classOf[MemberEvent])
+
+      Behaviors
+        .receiveMessage[ClusterDomainEvent] { event =>
+          event match {
+            case e @ MemberUp(member) =>
+              meterEvent(e, Some(member))
+              log.info("Member is Up: {}", member.address)
+            case e @ UnreachableMember(member) =>
+              meterEvent(e, Some(member))
+              log.info("Member detected as unreachable: {}", member)
+            case e @ MemberRemoved(member, previousStatus) =>
+              meterEvent(e, Some(member))
+              log.info("Member is Removed: {} after {}", member.address, previousStatus)
+            case e @ MemberDowned(member) =>
+              meterEvent(e, Some(member))
+              log.info("Member is Down: {}", member.address)
+            case e =>
+              meterEvent(e, None)
+              log.info(s"Cluster domain event: $e")
+          }
+          Behaviors.same
         }
-        Behaviors.same
-      }
-      .receiveSignal {
-        case (_, PreRestart) =>
-          cluster.subscriptions ! Subscribe(context.self, classOf[MemberEvent])
-          Behaviors.same
-        case (_, PostStop) =>
-          cluster.subscriptions ! Unsubscribe(context.self)
-          Behaviors.same
-      }
-  }
+        .receiveSignal {
+          case (_, PostStop) =>
+            cluster.subscriptions ! Unsubscribe(context.self)
+            Behaviors.same
+        }
+    }
 
   private def registerEvent(clusterEvents: Counter)(e: ClusterDomainEvent, member: Option[Member]): Unit =
     clusterEvents
